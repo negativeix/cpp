044bda9dbc25c9a81cc236920579ca6ece890721
Chitiwat Phajan <chitiwat2547@gmail.com>
2024-12-18 21:04:31 +0700

    first commit

---
+++ prac.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x;
+  string a="name";
+  //cin>>a[1];
+  cout << "Hello World!";
+  cout<<a;
+  
+  
+  
+  return 0;
+}
